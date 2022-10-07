4affa5b5229f56e1d73fc94e7022cfca5e127381
Brock Smith <brockricksmith@gmail.com>
2022-10-07 00:11:21 -0700

    First commit. Basic nvim files

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,33 @@
+local opts = { noremap = true, silent = true }
+
+local term_opts = { silent = true }
+
+local km = vim.api.nvim_set_keymap
+
+km("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+km("n", "<C-h>", "<C-w>h", opts)
+km("n", "<C-j>", "<C-w>j", opts)
+km("n", "<C-k>", "<C-w>k", opts)
+km("n", "<C-l>", "<C-w>l", opts)
+
+km("n", "<leader>e", ":Lex 30<cr>", opts)
+
+-- Resize with arrows
+km("n", "<C-Up>", ":resize +2<CR>", opts)
+km("n", "<C-Down>", ":resize -2<CR>", opts)
+km("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+km("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+
+-- Navigate buffers
+km("n", "<S-l>", ":bnext<CR>", opts)
+km("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Insert --
+-- Press jk fast to enter
+km("i", "jk", "<ESC>", opts)
+
